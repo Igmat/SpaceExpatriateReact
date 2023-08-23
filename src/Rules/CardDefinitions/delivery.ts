62e18f47859f6ab506d884224550e6848b280671
Anastasiia Zibla <zibla.asia@gmail.com>
2023-08-23 12:14:58 +0200

    feat deliverycards

--- src/Rules/CardDefinitions/delivery.ts
+++ src/Rules/CardDefinitions/delivery.ts
@@ -3,9 +3,12 @@ import { createCards } from "./createCards";
 
 export const deliveryCards = createCards<DeliveryCard>(
     "delivery",
-    { resources: ['biotic materials', 'biotic materials'] },
-    { resources: ['fuel', 'fuel'] },
-    { resources: ['minerals', 'minerals'] },
-    { resources: ['machinery'] },
-    { resources: ['nanotechnologies'] },
+    { resources: ['fuel', 'fuel'], quantity: 4 },
+    { resources: ['fuel', 'minerals'], quantity: 4 },
+    { resources: ['fuel', 'biotic materials'], quantity: 4 },
+    { resources: ['minerals', 'minerals'], quantity: 4 },
+    { resources: ['minerals', 'biotic materials'], quantity: 4 },
+    { resources: ['biotic materials', 'biotic materials'], quantity: 4 },
+    { resources: [ 'machinery'], quantity: 4 },
+    { resources: [ 'nanotechnologies'], quantity: 4 },
 )
