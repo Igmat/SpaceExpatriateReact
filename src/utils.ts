c1b7215f3926befd9634d55609269b6aac9965a5
KiraSeletska <seletskaya.kira@gmail.com>
2023-09-20 17:31:49 +0200

    fix: writeToLS

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,8 @@
 import localStorage from "mobx-localstorage";//new
 
-export const writeToLS = (key: string, value: any) =>{
+export const writeToLS = <T = undefined> (key: string, value: T ) =>{
     localStorage.setItem(key, value);
   }
-
 export const readFromLS = (key: string)  => (
     localStorage.getItem(key)
     )
