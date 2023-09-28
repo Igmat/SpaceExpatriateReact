815a07dc8edc1e89dc1fe0e167b8af1799209d48
Anastasiia Zibla <zibla.asia@gmail.com>
2023-09-28 11:23:29 +0200

    fix: Navlink instead of <a>

--- src/Pages/LoadGame/index.tsx
+++ src/Pages/LoadGame/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { gameStore } from "../../Store";
 import { observer } from "mobx-react-lite";
 import styles from "./LoadGame.module.scss";
+import { NavLink } from "react-router-dom";
 
 export const LoadGame = observer(() => {
   const { savedGameIds } = gameStore;
@@ -11,8 +12,8 @@ export const LoadGame = observer(() => {
       <h1>Load Your Game</h1>
       <ul className={styles.ul}>
         {savedGameIds.map((gameId, index) => (
-          <li key={index} className={styles.li}><a href={`/game/${gameId}`}>
-            {gameId}</a>
+          <li key={index} className={styles.li}><NavLink to={`/game/${gameId}`}>
+            {gameId}</NavLink>
             <button className={styles.smallbutton} onClick={() => gameStore.deleteSavedGameId(gameId)}>
               X
             </button>
