866d8c9347b1b87027c23b3dc809fe3be4be3043
Maria Angelova <angelova.m@hotmail.com>
2023-10-10 19:29:40 +0200

    test: updated snapshot

--- src/components/ColonyCard/index.tsx
+++ src/components/ColonyCard/index.tsx
@@ -1,8 +1,8 @@
-import { ColonyCard } from "../../Rules/card-types";
+import { ColonyCardWithPoints } from "../../Rules/Colony/ColonyDeckModel";
 import styles from "./colonyCard.module.scss";
 import { observer } from "mobx-react-lite";
 
-type CardProps = ColonyCard & {
+type CardProps = ColonyCardWithPoints & {
   onOpenCardClick?: () => void;
 };
 
@@ -12,7 +12,7 @@ export const CCard = observer((props: CardProps) => {
     <div className={styles.card} onClick={props.onOpenCardClick}>
       <div className={styles.mutateAction}>{props.mutateAction}</div>
       <div className={styles.benefit}>{props.benefit}</div>
-      <p className={styles.points}>2</p>
+      <p className={styles.points}>{props.points}</p>
       <p className={styles.colony}>COLONY</p>
       <p className={styles.name}>{props.name}</p>
     </div>
