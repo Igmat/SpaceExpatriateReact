d677740d2ca724e14323d6eb048548bfbc0ac1f5
Anastasiia Zibla <zibla.asia@gmail.com>
2023-10-11 10:59:25 +0200

    feat: #75

--- src/Player/Table/index.tsx
+++ src/Player/Table/index.tsx
@@ -24,15 +24,13 @@ export const Table = observer((props: TableProps) => {
     props.action.activateCardOnTable(card) &&
       setSelectedCards([...selectedCards, card]);
   };
- 
+
   return (
     <div className={styles.container}>
       <div className={styles.cardsContainer}>
         {props.model.colony.map((card, ind) => (
-          <CCard
-            key={ind}
-            {...card}
-          />))}
+          <CCard key={ind} {...card} />
+        ))}
       </div>
       <div className={styles.cardsContainer}>
         {props.model.delivery.map((card, ind) => (
@@ -78,48 +76,31 @@ export const Table = observer((props: TableProps) => {
           />
         ))}
       </div>
-      {props.round?.step === "performing" && (
-        <div className={styles.end} onClick={props.action.tryNext}>
-          End the round
-        </div>
-      )}
-      <div className={styles.round}>{"Round: " + props.round.current}</div>
-      {/*  {(["delivery", "engineering", "terraforming", "military"] as const).map(el => 
-        <div className={styles.cardsContainer}>
-        {props.model[el].map((card, ind) => (
-          <Card key={ind} {...card} 
-          {}
-          isSelected={props.action.cardsToDrop.includes(card)}
-           onClick={()=>props.action.activateCardsOnTable(card)}/>
-        ))}
-      </div>
-      )}*/}
 
-      {/*buttons*/}
-      {props.round.step === "performing" && selectedCards.length > 0 && (
-        <ResetButton action={props.action} />
-      )}
-      {props.round.phase === "delivery" &&
+      <div className={styles.round}>{"Round: " + props.round.current}</div>
+
+      {(props.round.phase === "delivery" ||
+        props.round.phase === "terraforming") &&
         props.round.step === "performing" && (
           <ResetButton action={props.action} />
         )}
-      {props.round.step === "performing"&& props.round.phase === "terraforming" && selectedCards.length >= 3 && (
-        <button
-          className={styles.confirmButton}
-          onClick={props.action.tryNext} //обнулить useState в пустой массив
-        >
-          Confirm
-        </button>
-      )}
-
-      {props.round.step === "performing" && (
-        <button
-          className={styles.giveUpButton}
-          onClick={props.action.tryNext} //обнулить useState в пустой массив
-        >
-          End turn
-        </button>
-      )}
+      {props.round.step === "performing" &&
+        (props.round.phase === "delivery" ||
+        props.round.phase === "terraforming" ? (
+          <button
+            className={styles.endTurnButton}
+            onClick={props.action.tryNext}
+          >
+            Confirm
+          </button>
+        ) : (
+          <button
+            className={styles.endTurnButton}
+            onClick={props.action.tryNext}
+          >
+            End turn
+          </button>
+        ))}
     </div>
   );
 });
