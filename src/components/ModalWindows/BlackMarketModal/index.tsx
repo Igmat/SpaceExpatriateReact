70e597e5e894fc8fbb7edef74dc509c4791e4e1e
Maria Angelova <angelova.m@hotmail.com>
2023-10-30 12:57:32 +0100

    feat: create black market colony modal

---
+++ src/components/ModalWindows/BlackMarketModal/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { ModalOptions } from "../../../Rules/ModalManager";
+import { DeliveryCard } from "../../../Rules/card-types";
+import styles from "./styles.module.scss";
+
+export const BlackMarketModal: FC<ModalOptions<DeliveryCard>> = (props) => {
+    console.log(props.params);
+    
+    return (
+        <div className={styles.modal}>
+            {props.params && props.params.map((card, id) => (
+                <div key={id} className={styles.modalDialog} onClick={() => props.onSelect(card)}>
+                    {card.resources.join(" + ")}
+                </div>
+            ))}
+        </div>
+    );
+    }
