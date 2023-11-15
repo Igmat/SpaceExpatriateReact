2a579dce83d3d8d4059d6bb6319bfea7ffc2ab9a
Maria Angelova <angelova.m@hotmail.com>
2023-11-15 17:40:35 +0100

    fix: delete unimplemedet methods

--- src/Rules/Places/TempDroppedCardsPlace.ts
+++ src/Rules/Places/TempDroppedCardsPlace.ts
@@ -6,12 +6,7 @@ import { CardType } from "../card-types";
 // Delivery Phase
 
 export class TempDroppedCardsPlace extends BasicPlace {
-    push(card: any) {
-        throw new Error("Method not implemented.");
-    }
-    forEach(arg0: (card: any) => any) {
-        throw new Error("Method not implemented.");
-    }
+    
     protected getCardInstance(id: number, type: CardType): BasicCard {
         return this.cardsCollection[type][id]
     }
